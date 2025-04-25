6376ba249df839c51262bf925055e99e6b7aa8c8
htoo-myat-maung <maung.htoomyat@gmail.com>
2025-04-25 14:46:45 -0500

    updating my forward chaining example

--- Forward Chaining/forward_chaining.py
+++ Forward Chaining/forward_chaining.py
@@ -1,12 +1,16 @@
 # forward chaining
 
 facts = {"browsed electronics", "another product","browsed fitness gear", "bought earbuds", "interested in health and fitness category"
-        , "not purcahsed smartwatch", "give positive review", "lives in warm climate", "prefer well-rated item"
+        , "not purchased smartwatch", "give positive review", "lives in warm climate", "prefer well-rated item"
         , "prefer mid-range item"}
 # inferred = set()    # set 
-rules = [({"browsed electronics", "another product"}, "interestd tech"),
-         ({"browsed fitness gear", "buys fitness products"}, "intersted in fitness"),
-         ({"interested tech", "interested in fitness"}, "interested in wearable tech")
+rules = [({"browsed electronics"}, "interestd in tech"),
+         ({"buys fitness products"}, "intersted in fitness"),
+         ({"browsed fitness gear"}, "interested in fitness"),
+         ({"interestd in tech", "interested in fitness"}, "interested in wearable tech"),
+         ({"interested in wearable tech", "not purchased smartwatch"}, "smarthwatches recommended"),
+         ({"smarthwatches recommended", "prefer mid-range item"}, "FitPlus Smart Band Pro"),
+         ({"smarthwatches recommended", "prefer high-range item", "prefer well-rated item", "lives in warm climate"}, "Galaxy Watch X")
         ]
 
         # another list
@@ -16,40 +20,40 @@ def forward_chain(f,r):
     all_facts = f
 
     while True:
-        new_inference = 0    #flag to see whether the new inference is added or not
+        #new_inference = 0    #flag to see whether the new inference is added or not
+        print("hello")
+        conditions = None
+        conclusion = None
         for x in r:
             conditions = x[0]
             conclusion = x [1]
             #print(f"IF {conditions} THEN {conclusion}")
 
-            print(conditions,conclusion)
+            
 
-            print(conditions.issubset(all_facts))
+            #print(conditions.issubset(all_facts))
 
-            if conditions.issubset(all_facts) and conclusion not in inferred:
-                print("here")
+            if conditions.issubset(all_facts) and conclusion not in set(inferred):
+                #print("here")
                 inferred.append(conclusion)
-                new_inference += 1
-                break
-        if new_inference > 0:
-            break
-        inferred.add(new_inference)
-        all_facts.add(new_inference)
+                all_facts.add(conclusion)
+                print(conditions,conclusion)
+                #print("here")
+
+
 
             # if conditions.issubset(facts) and conclusion not in inferred:
             #     print(f"Adding {conclusion} to inferred")
             #     inferred.add(conclusion)
             #     facts.append(conclusion)
+
         break
 
-
+    print(inferred)
 
     return inferred
 
 recommendation = forward_chain(facts, rules)
 
 
-#print the recommended products
-for i in recommendation:
-    print("\nfinally")
-    print(f"{i}")
+print(f"The recommendation for the customer is {recommendation[-1]}.")
